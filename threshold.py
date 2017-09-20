9d8ac0eab0f3f4b6e774307dbad61de62b84b320
LeeJaeHyuk92 <leejh920917@gmail.com>
2017-09-20 14:47:01 +0900

    threshold.py,  threshold & value

--- threshold.py
+++ threshold.py
@@ -5,8 +5,8 @@ import numpy as np
 
 # parameter to be modified
 debug = False
-thresh_min = 250
-thresh_max = 255
+thresh_min = 120
+thresh_max = 121
 
 
 path_img = 'images'
@@ -24,6 +24,7 @@ for thresh in range(thresh_min, thresh_max + 1, 1):
         cv2.imshow("threshold", img_bin)
         cv2.imshow("only_" + str(thresh) + "value", img_value)
         cv2.waitKey(0)
+
     cv2.imwrite(path_img + '/' + str(thresh) + '_threshold_' + str(name_img),
                 img_bin)
     cv2.imwrite(path_img + '/' + str(thresh) + '_value' + str(name_img),
